934bfa7704e30cbd6de13ef4b559329d181247a2
xViperAG <82969741+xViperAG@users.noreply.github.com>
2022-10-14 05:08:17 -0400

    Add files via upload

---
+++ [car-sounds]/sentinelsg4-sound/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+    'audioconfig/sentinelsg4_game.dat151.rel',
+    'audioconfig/sentinelsg4_sounds.dat54.rel',
+    'sfx/dlc_sentinelsg4/sentinelsg4.awc',
+    'sfx/dlc_sentinelsg4/sentinelsg4_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sentinelsg4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sentinelsg4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sentinelsg4'
