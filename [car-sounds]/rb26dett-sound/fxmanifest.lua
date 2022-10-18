d3c1cfa345469f42468a9113ea0705b393a31293
xViperAG <82969741+xViperAG@users.noreply.github.com>
2022-10-18 13:22:25 -0400

    Added more car sounds

---
+++ [car-sounds]/rb26dett-sound/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/rb26dett_game.dat151.rel',
+  'audioconfig/rb26dett_sounds.dat54.rel',
+  'sfx/dlc_rb26dett/rb26dett.awc',
+  'sfx/dlc_rb26dett/rb26dett_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
---
+++ [car-sounds]/s85b50-sound/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/s85b50_game.dat151.rel',
+  'audioconfig/s85b50_sounds.dat54.rel',
+  'sfx/dlc_s85b50/s85b50.awc',
+  'sfx/dlc_s85b50/s85b50_npc.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s85b50_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s85b50_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s85b50'
